0a0e0483971114af8fad889cd023b7148a8f6eef
Mohamed Hakkou <93436273+ImMrHak@users.noreply.github.com>
2024-05-24 01:02:24 +0100

    Initiation du projet StockMaster
    - Ajout de HELP.md pour la documentation du projet
    - Ajout de mvnw et mvnw.cmd pour l'exécution de Maven Wrapper
    - Ajout du fichier pom.xml pour la configuration Maven
    - Ajout de StockMasterApplication.java pour le démarrage de l'application
    - Ajout de application.properties pour la configuration des ressources
    - Ajout de StockMasterApplicationTests.java pour les tests unitaires

---
+++ StockMaster/api/src/main/java/com/project/StockMaster/StockMasterApplication.java
@@ -0,0 +1,12 @@
+package com.project.StockMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMasterApplication.class, args);
+	}
+}
